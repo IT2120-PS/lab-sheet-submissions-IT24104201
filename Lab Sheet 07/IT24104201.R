426ed706c9fe6e14f43573a3332ff17d5169c68d
IT24104201 <IT24104201@my.sliit.lk>
2025-09-17 12:46:11 +0530

    IT24104201_PS_Lab07

---
+++ Lab Sheet 07/IT24104201.R
@@ -0,0 +1,15 @@
+setwd("C:/Users/User/Desktop/IT24104201")
+getwd()
+
+#01
+punif(25,min=0,max=40,lower.tail=TRUE) - punif(10,min=0,max=40,lower.tail=TRUE)
+
+#02
+pexp(2,rate=1/3,lower.tail=TRUE)
+
+#03(i)
+pnorm(130,mean=100,sd=15,lower.tail=FALSE)
+
+#03(ii)
+qnorm(0.95,mean=100,sd=15)
+
